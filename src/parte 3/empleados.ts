a542cef935338f7bb7868901806d738e4a55c3d2
Joaquin Wener <joaquinwener@gmail.com>
2026-09-07 19:43:02 -0300

    ejercicio 13 parte 2 hecho y testeado

--- src/parte 3/empleados.ts
+++ src/parte 3/empleados.ts
@@ -25,6 +25,7 @@ export class EmpleadoTiempoCompleto extends Empleado {
     }
 
     calcularSueldo(): number {
+        return this.sueldoBasico;
         // TODO: cobra el sueldo básico, sin más cálculo.
         throw new Error("Implementar");
     }
@@ -42,6 +43,7 @@ export class EmpleadoMedioTiempo extends Empleado {
     }
 
     calcularSueldo(): number {
+        return this.horasTrabajadas * this.valorHora;
         // TODO: sueldo = horasTrabajadas * valorHora
         throw new Error("Implementar");
     }
@@ -59,6 +61,7 @@ export class EmpleadoPorComision extends Empleado {
     }
 
     calcularSueldo(): number {
+        return this.ventasTotales * (this.porcentajeComision / 100);
         // TODO: sueldo = ventasTotales * (porcentajeComision / 100)
         throw new Error("Implementar");
     }
